a451f62ef260a3cf483bff95b3282afdec15ebe4
4l3ss <2623600+4l3ss@users.noreply.github.com>
2022-11-04 16:24:05 +0100

    feat: add preload for config (#12)
    This commit adds a preload for the config endpoint.
    
    The config endpoint is used to fetch the configuration for the frontend.
    
    This is a performance improvement because the config is fetched before the
    frontend is loaded.

--- butanuki-front/src/setupProxy.js
+++ butanuki-front/src/setupProxy.js
@@ -1,6 +1,12 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
+  app.use(function (req, res, next) {
+    if (req.url === "/") {
+      res.setHeader("Link", '</api/config>; rel="preload"; as="fetch"');
+    }
+    next();
+  });
   app.use(
     "/api",
     createProxyMiddleware({
