c6ca6b1ec47dc03072eed335d6610b556c1b21aa
4l3ss <2623600+4l3ss@users.noreply.github.com>
2022-12-01 16:48:17 +0100

    [Front] Code split (#24)

--- butanuki-front/src/pages/app/Estimator/EstimatorPage.tsx
+++ butanuki-front/src/pages/app/Estimator/EstimatorPage.tsx
@@ -1,8 +1,13 @@
 import { MainLayout } from "../../../layout/MainLayout";
-import { SavingEstimator } from "../../../components/DCAEstimator/SavingEstimator";
 import { SavingEstimatorConfigProvider } from "../../../components/DCAEstimator/SavingEstimatorConfigProvider";
 import { useTranslation } from "react-i18next";
 import { usePageTitle } from "../../../utils/hooks";
+import { LoadingCard } from "../../../components/LoadingCard";
+import React, { Suspense } from "react";
+
+const SavingEstimator = React.lazy(
+  () => import("../../../components/DCAEstimator/SavingEstimator")
+);
 
 export const EstimatorPage = () => {
   const { t } = useTranslation();
@@ -11,7 +16,9 @@ export const EstimatorPage = () => {
     <MainLayout>
       <h1 className={"heading-bitcoin text-center"}>{t("estimator.title")}</h1>
       <SavingEstimatorConfigProvider>
-        <SavingEstimator />
+        <Suspense fallback={<LoadingCard />}>
+          <SavingEstimator />
+        </Suspense>
       </SavingEstimatorConfigProvider>
     </MainLayout>
   );
