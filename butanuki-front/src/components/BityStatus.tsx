94c2bfbe3dc61e13373b60bf0832dcf4a525362c
4l3ss <2623600+4l3ss@users.noreply.github.com>
2023-02-02 16:47:07 +0100

    Add bity register link (#49)

--- butanuki-front/src/components/BityStatus.tsx
+++ butanuki-front/src/components/BityStatus.tsx
@@ -1,16 +1,33 @@
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { BityLinkStatus, TokenStatus } from "../generated/graphql";
 import { LinkBityBtn, UnlinkBityBtn } from "./buttons/BityBtn/LinkBityBtn";
 import React from "react";
 
 export const BityStatus = React.memo(
   ({ bityStatus }: { bityStatus: BityLinkStatus }) => {
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
 
     const linkBtn = (
       <p>
         {t("app.home.bity.not_linked")} <LinkBityBtn />{" "}
         {t("app.home.bity.to_link")}
+        <br />
+        <Trans
+          i18nKey="app.home.bity.create_bity"
+          t={t}
+          i18n={i18n}
+          components={{
+            link1: (
+              // eslint-disable-next-line jsx-a11y/anchor-has-content
+              <a
+                className="btn btn-sm btn-success"
+                href={"https://buy.btnk.ch"}
+                target={"_blank"}
+                rel="noreferrer"
+              />
+            ),
+          }}
+        />
       </p>
     );
     const unlinkBtn = (
